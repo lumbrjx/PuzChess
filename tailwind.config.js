c84921e47838087a361f3fbfdc8fa984e23991c1
CLog <88345999+CLOG9@users.noreply.github.com>
2023-06-29 17:55:23 +0200

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        clrPrimaryBlack: "hsl(var(--color-primary-black) / <alpha-value>)",
+        clrSecondaryGrey: "hsl(var(--clr-secondary-grey) / <alpha-value>)",
+        clrFont: "hsl(var(--clr-font) / <alpha-value>)",
+        clrLayoutGreen: "hsl(var(--clr-layout-green) / <alpha-value>)",
       },
     },
   },
   plugins: [],
-}
+};
