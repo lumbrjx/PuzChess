29c65542dcfcba792d30414fe2765374f613b6e0
CLog <88345999+CLOG9@users.noreply.github.com>
2023-06-29 19:19:11 +0200

    tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,17 @@ module.exports = {
         clrFont: "hsl(var(--clr-font) / <alpha-value>)",
         clrLayoutGreen: "hsl(var(--clr-layout-green) / <alpha-value>)",
       },
+      fontSize: {
+        largeFnt: "3.4375rem", //for landing/main page section headings,
+        bigFnt: "2.25rem", //for page title, subtitles, players names,blogs titles
+        mediumFnt: "1.71875rem", //labels, buttons, some discriptions
+        smallFnt: "1.5rem", // blog body, header/footer links, placeholders
+      },
+      fontWeight: {
+        lightFnt: 400, // header/footer links,placeholders
+        midFnt: 500, // buttons, page title, subdiscription ,players names,labels,
+        boldFnt: 600, //for landing/main page section headings,blogs
+      },
     },
   },
   plugins: [],
