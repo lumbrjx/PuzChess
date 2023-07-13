6ab90812ca31a070a4aa52fc66271abbc2339bd9
Tayeb <88345999+CLOG9@users.noreply.github.com>
2023-07-14 00:44:46 +0200

    Update user.ts

--- src/server/db/data/users/user.ts
+++ src/server/db/data/users/user.ts
@@ -10,7 +10,7 @@ export const createUser = async (
 ) => {
   try {
     const user = await prisma.user.create({
-      data: <userType>{
+      data: any{
         name,
         password,
         email,
