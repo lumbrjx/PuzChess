4a7d1059167a2b1b2df72fd29c584a928ccd734b
CLog <88345999+CLOG9@users.noreply.github.com>
2023-08-03 06:10:41 +0200

    front dash ui

---
+++ src/components/ui/stats.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Stats = () => {
+  return (
+    <div
+      className="text-mediumFnt bg-clrPrimaryBlack
+    rounded-2xl w-full max-w-[20rem] border border-1 border-clrSecondaryGrey shadow-cardDrop  py-12 px-4 sm:p-12"
+    >
+      <h2 className="font-boldFnt text-mediumF mb-4">Server stats</h2>
+      <p>Users: </p>
+      <p>Blogs: </p>
+    </div>
+  );
+};
+
+export default Stats;
