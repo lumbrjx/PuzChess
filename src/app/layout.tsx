74692e2a2a2093e0607788d5187641f8f38c0c59
CLog <88345999+CLOG9@users.noreply.github.com>
2023-08-08 04:33:04 +0200

    og2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata = {
     siteName: "PuzChess",
     images: [
       {
-        url: "https://puzchess.vercel.app/public/webOg.png",
+        url: "https://puzchess.vercel.app/opengraph-image.jpg",
         width: 1200,
         height: 600,
         alt: "logo",
@@ -33,7 +33,7 @@ export const metadata = {
   },
   images: [
     {
-      url: "https://puzchess.vercel.app/public/webOg.png",
+      url: "https://puzchess.vercel.app/opengraph-image.jpg",
       width: 1200,
       height: 600,
       alt: "logo",
