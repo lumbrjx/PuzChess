b17acb4cb87b8ad4028d6d794c85a625943541f6
Tayeb <88345999+CLOG9@users.noreply.github.com>
2023-08-08 17:29:01 +0200

    Update fetchPuzzle.ts

--- src/lib/hooks/query/fetchPuzzle.ts
+++ src/lib/hooks/query/fetchPuzzle.ts
@@ -12,7 +12,7 @@ export const useFetchPuzzle = () => {
         playerMoves: "4",
       },
       headers: {
-        "X-RapidAPI-Key": process.env.X_RapidAPI_Key,
+        "X-RapidAPI-Key": process.env.X_RAPIDAPI_KEY,
         "X-RapidAPI-Host": "chess-puzzles.p.rapidapi.com",
       },
     };
